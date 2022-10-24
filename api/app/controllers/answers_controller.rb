2513c71a80845a645cb2700c68ebc5eef280f2ee
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-10-24 21:10:43 +0300

    CRUD methods

--- api/app/controllers/answers_controller.rb
+++ api/app/controllers/answers_controller.rb
@@ -1,2 +1,36 @@
 class AnswersController < ApplicationController
+
+    #display all answers in the questions
+    def index
+      answers = Answer.all
+      render json: answers
+    end
+
+    #view answer per question
+    def show
+        answer = Answer.find(params[:id])
+        render json: answer
+    end
+
+    #create answers 
+    def create
+        answer = Answer.create(params_answer)
+        render json: answer
+    end
+
+    #update answers
+    def update
+        answer = Answer.find(params[:id])
+        answer.update(update_answer)
+        render json: answer
+    end
+
+    private
+    def params_answer
+        params.permit(:question_id, :answer1, :answer2, :answer3, :answer4)
+    end
+
+    def update_answer
+        params.permit(:answer1, :answer2, :answer3, :answer4)
+    end
 end
--- api/app/controllers/questions_controller.rb
+++ api/app/controllers/questions_controller.rb
@@ -1,2 +1,36 @@
 class QuestionsController < ApplicationController
+
+    #display all questions
+    def index
+        questions = Question.all
+        render json: questions
+    end
+
+    #find one question
+    def show
+        question = Question.find(params[:id])
+        render json: question
+    end
+
+    #create a new question
+    def create
+        question = Question.create(params_quiz)
+        render json: question
+    end
+
+    #update a question
+    def update
+        question = Question.find(params[:id])
+        quetion.update(update_quiz)
+        render json: question
+    end
+
+    private
+    def update_quiz
+        params.permit(:quiz)
+    end
+
+    def params_quiz
+        params.permit(:question, :answer_id)
+    end
 end
