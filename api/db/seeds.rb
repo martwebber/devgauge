985e8d6ce36d8b460014727f459fc662d7fe3e9d
oyayafelix <oyayafelix@gmail.com>
2022-10-25 11:27:15 +0300

    updated params values

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Assessment.create = (status: 0, user_id: '1', duration:"20", is_actual:"true", assessment_date: "2022-3-2", start_time: "2022-3-2", assessment_type: "Python")
-Assessment.create = (status: 1, user_id: '2', duration:"10", is_actual:"false", assessment_date: "2022-4-2", start_time: "2023-9-4", assessment_type: "Ruby on rails")
-Assessment.create = (status: 0, user_id: '3', duration:"50", is_actual:"true", assessment_date: "2022-3-2", start_time: "2022-6-9", assessment_type: "Bootstrap")
+#Assessment.create = (status: 0, user_id: '1', duration:"20", is_actual:"true", assessment_date: "2022-3-2", start_time: "2022-3-2", assessment_type: "Python")
+#Assessment.create = (status: 1, user_id: '2', duration:"10", is_actual:"false", assessment_date: "2022-4-2", start_time: "2023-9-4", assessment_type: "Ruby on rails")
+#Assessment.create = (status: 0, user_id: '3', duration:"50", is_actual:"true", assessment_date: "2022-3-2", start_time: "2022-6-9", assessment_type: "Bootstrap")
