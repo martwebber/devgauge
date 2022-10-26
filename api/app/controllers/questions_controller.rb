37cdd83be0914ac817d577eea44753753722edcf
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-10-26 14:59:24 +0300

    added topic_id to question table

--- api/app/controllers/questions_controller.rb
+++ api/app/controllers/questions_controller.rb
@@ -61,7 +61,7 @@ class QuestionsController < ApplicationController
     end
 
     def params_quiz
-        params.permit(:quiz, :correct_answer)
+        params.permit(:quiz, :correct_answer, :topic_id)
     end
 
 end
--- api/app/models/question.rb
+++ api/app/models/question.rb
@@ -3,4 +3,5 @@ class Question < ApplicationRecord
     has_many :assessments , through: :assessment_questions
     has_many :answers, dependent: :destroy
     has_many :student_answers
+    belongs_to :topic
 end
