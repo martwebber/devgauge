c051f752646bd64edc22d7bf6d85c2d8cb4f549b
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-10-27 16:40:09 +0300

    POST questions form

--- frontend/src/pages/assessments/index.jsx
+++ frontend/src/pages/assessments/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import CreateNewQuestionForm from '../questions/CreateNewQuestionForm'
+import CreateAssessmentForm from './CreateAssessmentForm'
 
 export const Assessments = () => {
   return (
-    <div>Assessments</div>
+    <div>
+      <div>Assessments</div>
+      <div>
+      <CreateNewQuestionForm/>
+      </div><br/><br/>
+      <div>
+      <CreateAssessmentForm />
+      </div>
+      
+      
+    </div>
+    
   )
 }
