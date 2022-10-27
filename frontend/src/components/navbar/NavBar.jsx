fef92288e7f0a0b930f8ccefbc0ffc64e26f77be
oyayafelix <oyayafelix@gmail.com>
2022-10-27 21:16:26 +0300

    nav bar styling

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -10,11 +10,11 @@ export const NavBar = () => {
     return (
     <Navbar className='navbar'expand="lg">
     <Container>
-      <Navbar.Brand as={Link} to="/">devGauge</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/" className='main'>devGauge</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="me-automy-2my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
@@ -24,7 +24,7 @@ export const NavBar = () => {
         <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
         <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        <Nav.Link as={Link} to="/login"className='login-btn' >Login  </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -33,7 +33,7 @@ export const NavBar = () => {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button className='search-btn' >Search</Button>
           </Form>
         </Navbar.Collapse>
     </Container>
